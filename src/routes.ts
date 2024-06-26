7cd0974407a81c082ebd08efb511996588057548
felipe-bon <felipebonetti2001@hotmail.com>
2024-06-26 13:39:08 -0300

    Methods for update, delete, and getById added to the Product Controller and corresponding routes included.

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,14 @@ import { DepartmentController } from "./controllers/DepartmentsController";
 
 const routes = Router()
 
-routes.get('/products/:departmentName', new ProductController().getProductsByDepartment)
+routes.get('/products/:id', new ProductController().getProduct)
 routes.post('/products', new ProductController().create)
+routes.put('/products/:id', new ProductController().updateProduct)
 routes.get('/products', new ProductController().getAll)
+routes.delete('/products/:id', new ProductController().deleteProduct)
 
 routes.post('/departments', new DepartmentController().create)
 routes.get('/departments', new DepartmentController().getAll)
+routes.get('/departments/:departmentName', new ProductController().getProductsByDepartment)
 
 export default routes 
