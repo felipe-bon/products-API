694b9e935ddf09dbd42b4c5562cde75f87080662
felipe-bon <felipebonetti2001@hotmail.com>
2024-06-30 00:48:36 -0300

    Removed try-catch blocks from ProductsController and DepartmentsController, now using the created error-handling middleware

--- src/controllers/DepartmentsController.ts
+++ src/controllers/DepartmentsController.ts
@@ -2,109 +2,72 @@ import { Request, Response } from "express";
 import { DepartmentRepository } from "../repositories/departmentRepository";
 import { productRepository } from "../repositories/productRepository";
 import { In } from "typeorm";
+import { NotFoundError } from "../helpers/api-errors";
 
 
 export class DepartmentController{
     async create(req: Request, res: Response){
 
         const {department_name} = req.body
-        const {department_id} = req.params
         
-        try{
+        const newDepartment = DepartmentRepository.create({department_name})
+        await DepartmentRepository.save(newDepartment)
 
-            const newDepartment = DepartmentRepository.create({department_name})
-            await DepartmentRepository.save(newDepartment)
-
-            return res.status(201).json(newDepartment)
-
-        } catch(error){
-            console.log(error);
-            return res.status(500).json({message: 'Internal Server Error'})
-        }
-
+        return res.status(201).json(newDepartment)
     }
 
     async getAll(req: Request, res: Response){
 
-
-        try{
-
-            const departments = await DepartmentRepository.find();
-            return res.status(200).json(departments)
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({message: 'Internal Server Error'})
-        }
-
+        const departments = await DepartmentRepository.find();
+        return res.status(200).json(departments)
     }
 
     async updateDepartment(req: Request, res: Response){
         const { id } = req.params;
         const { name} = req.body;
 
-        try{
-            const departmentId = parseInt(id);
-            const department = await DepartmentRepository.findOneBy({id: departmentId});
+        const departmentId = parseInt(id);
+        const department = await DepartmentRepository.findOneBy({id: departmentId});
 
-            if(!department){
-                return res.status(404).json({message:'Department not found'});
-            }
-
-            if(name) department.department_name = name
-
-            await DepartmentRepository.save(department);
-
-            return res.status(200).json(department);
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({message: 'Internal Server Error'})
+        if(!department){
+            throw new NotFoundError('Department not found')
         }
+
+        if(name) department.department_name = name
+
+        await DepartmentRepository.save(department);
+
+        return res.status(200).json(department);
+
     }
 
     async getDepartment(req: Request, res: Response){
         const { id } = req.params
 
-        try{
-            const departmentId = parseInt(id);
+        const departmentId = parseInt(id);
             
-            const department = await DepartmentRepository.findOneBy({id: departmentId});
+        const department = await DepartmentRepository.findOneBy({id: departmentId});
 
-            if(!department){
-                return res.status(404).json({message:'Department not found'});
-            }
-
-            return res.json(department);
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({message: "Internal server error"});
+        if(!department){
+            throw new NotFoundError('Department not found')
         }
+
+        return res.json(department);
+
     }
 
     async deleteDepartment(req: Request, res: Response){
             
         const { id } = req.params;
+        const departmentId = parseInt(id);
+        const department = DepartmentRepository.findOneBy({id: departmentId});
 
-        try{
-            const departmentId = parseInt(id);
-
-            const department = DepartmentRepository.findOneBy({id: departmentId});
-
-            if(!department){
-                return res.status(404).json({message:'Department not found'});
-            }
-
-            await DepartmentRepository.delete(id);
-
-            return res.status(200).json({message:'Department deleted'});
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({message: "Internal server error"});
+        if(!department){
+            throw new NotFoundError('Department not found')
         }
 
+        await DepartmentRepository.delete(id);
 
+        return res.status(200).json({message:'Department deleted'});
     }
 }
--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { productRepository } from "../repositories/productRepository";
 import { DepartmentRepository } from "../repositories/departmentRepository";
 import { In } from "typeorm";
+import { BadRequestError, NotFoundError } from "../helpers/api-errors";
 
 export class ProductController {
     
@@ -9,67 +10,46 @@ export class ProductController {
         const { name, description, barcode, price, departmentIds } = req.body;
 
         if (!name || !departmentIds || departmentIds.length === 0) {
-            return res.status(400).json({ message: 'Name and at least one departmentId are required' });
+            throw new BadRequestError('Name and at least one departmentId are required')
         }
 
-        try {
-
-            const departments = await DepartmentRepository.findByIds(departmentIds);
-            if (departments.length !== departmentIds.length) {
-                return res.status(404).json({ message: 'One or more departments not found' });
-            }
-
-            const newProduct = productRepository.create({
-                name,
-                description,
-                barcode,
-                price,
-                departments
-            });
-            
-
-            await productRepository.save(newProduct);
-
-            return res.status(201).json(newProduct);
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'Internal server error' });
+        const departments = await DepartmentRepository.findByIds(departmentIds);
+        if (departments.length !== departmentIds.length) {
+            throw new NotFoundError('One or more departments not found')
         }
+
+        const newProduct = productRepository.create({
+            name,
+            description,
+            barcode,
+            price,
+            departments
+        });
+        
+
+        await productRepository.save(newProduct);
+
+        return res.status(201).json(newProduct);
     }
 
     async getAll(req: Request, res: Response){
         
-        try{
-
-            const products = await productRepository.find()
-
-            return res.status(200).json(products);
-
-        } catch(error){
-            console.log(error)
-            return res.status(500).json({message: 'Internal server error'});
-        }
+        const products = await productRepository.find()
+        return res.status(200).json(products);
     }
 
     async getProduct(req: Request, res: Response){
         const { id } = req.params;
 
-        try{
+        const productId = parseInt(id);
 
-            const productId = parseInt(id);
-
-            const product = await productRepository.findOneBy({id: productId});
-            
-            if(!product){
-                return res.status(404).json('Product not found')
-            }
-
-            return res.status(200).json(product)
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({message: 'Internal Server Error'});
+        const product = await productRepository.findOneBy({id: productId});
+        
+        if(!product){
+            throw new NotFoundError('Product not found')
         }
+
+        return res.status(200).json(product)
 
     }
 
@@ -78,90 +58,66 @@ export class ProductController {
         const { id } = req.params;
         const { name, description, barcode, price, departmentIds } = req.body;
 
-        try {
-            const productId = parseInt(id);
-            const departmentIdsInt = departmentIds.map((i: { id: string; }) => parseInt(i.id));
+        const productId = parseInt(id);
+        const departmentIdsInt = departmentIds.map((i: { id: string; }) => parseInt(i.id));
 
-            const product = await productRepository.findOne({
-                where: { id: productId },
-                relations: ["departments"]
-            });
+        const product = await productRepository.findOne({
+            where: { id: productId },
+            relations: ["departments"]
+        });
 
-
-            if(!product){
-                return res.status(404).json({message:"Product not found"});
-            }
-
-            // verifica se os deparments existem.
-
-            const departments = await DepartmentRepository.findBy({
-                id: In (departmentIdsInt)
-            })
-
-            
-            if(!departments || (Object.values(departments)).length != departmentIdsInt.length){
-                return res.status(404).json({message:'One or more departments not found'});
-            }
-
-            if (name) product.name = name;
-            if (description) product.description = description;
-            if (barcode) product.barcode = barcode;
-            if (price) product.price = price;
-            product.departments = departments;
-
-            await productRepository.save(product);
-
-            return res.status(200).json(product);
-
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'Internal Server Error' });
+        if(!product){
+            throw new NotFoundError('Product not found')
         }
+
+        // verifica se os deparments existem.
+
+        const departments = await DepartmentRepository.findBy({
+            id: In (departmentIdsInt)
+        })
+
+        
+        if(!departments || (Object.values(departments)).length != departmentIdsInt.length){
+            throw new NotFoundError('One or more departments not found')
+        }
+
+        if (name) product.name = name;
+        if (description) product.description = description;
+        if (barcode) product.barcode = barcode;
+        if (price) product.price = price;
+        product.departments = departments;
+
+        await productRepository.save(product);
+
+        return res.status(200).json(product);
+
     }
 
 
     async getProductsByDepartment(req: Request, res: Response) {
         const { departmentName } = req.params;
 
-        try {
-
-            const department = await DepartmentRepository.findOne({ relations: ['products'], where: { department_name: departmentName } });
-            if (!department) {
-                return res.status(404).json({ message: 'Department not found' });
-            }
-
-            return res.json(department.products);
-
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'Internal server error' });
+        const department = await DepartmentRepository.findOne({ relations: ['products'], where: { department_name: departmentName } });
+        if (!department) {
+            throw new NotFoundError('Department not found')
         }
+
+        return res.json(department.products);
     }
 
     async deleteProduct(req: Request, res: Response){
 
         const { id } = req.params
         
+        const productId = parseInt(id)
+        const product = await productRepository.findOneBy({id: productId});
 
-        try{
-
-            const productId = parseInt(id)
-            const product = await productRepository.findOneBy({id: productId});
-
-            if (!product){
-                return res.status(404).json('Product not nound');
-            }
-
-            await productRepository.delete(id)
-
-            return res.status(200).json({message:'Product deleted'});
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({ message: 'Internal Server Error' });
+        if (!product){
+            throw new NotFoundError('Product not nound')
         }
 
+        await productRepository.delete(id)
 
+        return res.status(200).json({message:'Product deleted'});
     }
-
 }
